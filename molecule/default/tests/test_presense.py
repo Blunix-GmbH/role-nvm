aedb5aaa249b5be0066f5779606e1c8282fd1d0f
Peter Thurner <p.thurner@blunix.org>
2019-06-13 17:29:20 +0200

    added test

--- molecule/default/tests/test_presense.py
+++ molecule/default/tests/test_presense.py
@@ -5,9 +5,6 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-PHP_FPM_VERSION = '7.1'
 
-
-def test_executable_exists(host):
-    # TODO shouldnt this be in users home?
-    assert host.file('/usr/bin/node').exists
+def test_nodejs_pkg_exists(host):
+    assert host.file('/var/www/www_example_com/node_modules/fo/fo.js').exists
